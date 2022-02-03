fe321765ece9a49b06356c5980787bf7dfa6e038
andrethiele <thielea@th-brandenburg.de>
2022-02-03 11:45:54 +0100

    test failing build

--- src/test/kotlin/app/graphkt/AppTests.kt
+++ src/test/kotlin/app/graphkt/AppTests.kt
@@ -7,7 +7,7 @@ class AppTests {
 
 	@Test
 	fun test_schema_name() {
-		val actual = buildSchema("MySchema") {}.name
+		val actual = buildSchema("MySchema1") {}.name
 		val expected = "MySchema"
 		assertEquals(expected, actual)
 	}
